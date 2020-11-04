c59f3b3381479f3cf851d2713519613a839372b0
florian.charpentier <florian.charpentier@epita.fr>
2020-11-04 15:11:19 +0100

    init front

---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Navbar></Navbar>
+    <div id="content">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Navbar from "@/components/Navbar";
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+  }
+}
+</script>
+
+<style>
+  #app {
+
+  }
+  #content {
+    margin: 15px
+  }
+
+  .p {
+
+  }
+</style>
---
+++ frontend/src/components/cartography/Filters.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        menu deroulant here
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Filters"
+    }
+</script>
+
+<style scoped>
+
+</style>
