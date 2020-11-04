b7654dacc8eff3d6abf821064873a8fad9acf4a4
Baptistin Boilot <baptistin.boilot@epita.fr>
2020-11-04 21:55:15 +0100

    Add production environment on backend

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -15,19 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xbve^2mqly5z#@$*-iyinv5o@y885pzm&1@tc%*%f9)q9@)-*='
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["vps-2ea52359.vps.ovh.net", "localhost", "127.0.0.1"]
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -73,18 +60,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -107,7 +82,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
 
 TIME_ZONE = 'UTC'
 
---
+++ backend/backend/settings/prod.py
@@ -0,0 +1,21 @@
+from .base import *
+
+DEBUG = False
+SECRET_KEY = os.environ['SECRET_KEY']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+ALLOWED_HOSTS = ["vps-2ea52359.vps.ovh.net", "localhost", "127.0.0.1"]
+
+# CORS
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://vps-2ea52359.vps.ovh.net/',
+    'https://vps-2ea52359.vps.ovh.net/#/'
+]
