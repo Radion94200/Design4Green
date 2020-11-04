ff8517df0cea86cd22e1c5883edf5b9dd5600fcb
Baptistin Boilot <baptistin.boilot@epita.fr>
2020-11-04 21:55:15 +0100

    Add production environment on backend

---
+++ backend/backend/settings/dev.py
@@ -0,0 +1,19 @@
+from .base import *
+
+DEBUG = True
+SECRET_KEY = '=$^ot$yl64fkh=ls2s+q6a1(w=z(3w+s%+gob=g+ped3uby0y0'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+
+# CORS
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
