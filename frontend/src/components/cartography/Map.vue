58cfa538015d5afbcdb53a1ea839b7b1d252b26b
florian.charpentier <florian.charpentier@epita.fr>
2020-11-05 10:47:15 +0100

    working service

--- frontend/src/components/cartography/Map.vue
+++ frontend/src/components/cartography/Map.vue
@@ -23,12 +23,12 @@
 
     #map-content {
         padding: 20px;
-        border: solid 5px darkgreen;
+        border: solid 5px black;
         border-radius: 10px;
 
         display: grid;
         align-items: center;
 
-        background-color: lightgreen;
+        background-color: lightgrey;
     }
 </style>
