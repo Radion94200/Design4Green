5a7f453bfc3b08bd4f4a9c8efcb3f4eea0533783
florian.charpentier <florian.charpentier@epita.fr>
2020-11-05 10:14:03 +0100

    fixed CORS #4

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
--- backend/backend/settings/dev.py
+++ backend/backend/settings/dev.py
@@ -14,6 +14,6 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 # CORS
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
+    'http://localhost:8080',
     'http://127.0.0.1:8080',
 ]
